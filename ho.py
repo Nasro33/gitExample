1e76c12bb670749f621b00a5b9e1435cbed270b2
Nasro33 <namo23@student.bth.se>
2025-09-19 10:14:53 +0200

    committar

--- ho.py
+++
@@ -1,8 +0,0 @@
-def sum(a, b):
-    return a + b
-
-
-
-
-print(sum(3, 5))
-print("ho.py is being run directly")
---
+++ test.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    return a + b
+
+
+
+
+print(sum(3, 5))
+print("ho.py is being run directly")
